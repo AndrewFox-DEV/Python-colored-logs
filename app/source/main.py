d2659b894939e5632fc58a49194c74c77c043beb
AndrewFox_DEVELOPER <114300206+AndrewFox-DEV@users.noreply.github.com>
2023-03-24 23:29:58 +0100

    Update main.py

--- app/source/main.py
+++ app/source/main.py
@@ -1,3 +1,13 @@
 from logs import Logs
 
-# Happy programming!
+# [TAG] Text here..
+print(Logs.success_log(tag='SUCCESS', text='This is a log!'))
+print(Logs.warning_log(tag='WARNING', text='This is a log!'))
+print(Logs.error_log(tag='ERROR', text='This is a log!'))
+print(Logs.info_log(tag='INFO', text='This is a log!'))
+
+# Text here..
+print(Logs.success_log(text='This is a log!'))
+print(Logs.warning_log(text='This is a log!'))
+print(Logs.error_log(text='This is a log!'))
+print(Logs.info_log(text='This is a log!'))
